73d3d27e335424ce28ec2e3c122732513d5036bb
Juanes-GH <jjgohe3@gmail.com>
2021-07-16 08:15:55 +0200

    I have install all the librerys that I'll need

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-const express = require('express');
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+
 const app = express();
 
+// Middleware
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", (req, res)=>{
-    console.log('hello wolrd')
-})
+// Rutas
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+// Middleware para Vue.js router modo history
+//const history = require('connect-history-api-fallback');
+//app.use(history());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('puerto', process.env.PORT || 3000);
+app.listen(app.get('puerto'), () => {
+  console.log('Example app listening on port'+ app.get('puerto'));
 });
